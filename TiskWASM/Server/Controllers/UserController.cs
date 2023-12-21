2f2963fd47cfb77d515915afe55a137bf484f5ce
Smegfi <68690815+Smegfi@users.noreply.github.com>
2023-12-21 07:02:20 +0100

    updt

--- TiskWASM/Server/Controllers/UserController.cs
+++ TiskWASM/Server/Controllers/UserController.cs
@@ -1,7 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Globalization;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
 using TiskWASM.Server.Data.Repositories;
 using TiskWASM.Shared;
+using TiskWASM.Shared.Csv;
 
 namespace TiskWASM.Server.Controllers
 {
@@ -10,9 +18,13 @@ namespace TiskWASM.Server.Controllers
     public class UserController : ControllerBase
     {
         private UserRepository repository;
-        public UserController(IConfiguration config)
+        private readonly IWebHostEnvironment env;
+        private readonly IConfiguration configuration;
+        public UserController(IConfiguration config, IWebHostEnvironment env)
         {
             this.repository = new UserRepository(config);
+            this.configuration = config;
+            this.env = env;
         }
 
         [HttpGet]
@@ -26,6 +38,7 @@ namespace TiskWASM.Server.Controllers
         {
             return await this.repository.FindByEmail(filter);
         }
+
         [HttpGet("{filter:int}")]
         public async Task<dtUser> FindById(int filter)
         {
@@ -36,6 +49,64 @@ namespace TiskWASM.Server.Controllers
         public async Task Create(dtUser model)
         {
             await repository.CreateAsync(model);            
+        }
+
+        [HttpPost("upload")]
+        public async Task PostFile([FromForm] IFormFile file)
+        {
+            var path = Path.Combine(configuration.GetValue<string>("FileUploads"), file.FileName);
+
+            await using FileStream fs = new(path, FileMode.Create);
+            await file.CopyToAsync(fs);
+
+            await fs.FlushAsync();
+            fs.Close();
+
+            await FromFileToDatabase(path);
+        }
+
+        private async Task FromFileToDatabase(string filename)
+        {
+            List<csvKontakt> users = new List<csvKontakt>();
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+                Encoding = Encoding.UTF8,   
+                BadDataFound = null,
+                Delimiter = ";"
+            };
+            using (var reader = new StreamReader(filename))
+            using (var csv = new CsvReader(reader, config))
+            {
+                var records = new List<csvKontakt>();
+                csv.Read();
+                csv.ReadHeader();
+                while (csv.Read())
+                {
+                    var record = new csvKontakt()
+                    {
+                        Name = csv.GetField<string>("Jméno"),
+                        Surname = csv.GetField<string>("Příjmení"),
+                        Email = csv.GetField<string>("El. adresa"),
+                        Department = csv.GetField<string>("Odbor"),
+                        Office = csv.GetField<string>("Kancelář")
+                    };
+                    users.Add(record);
+                }
+            }
+
+            foreach (var item in users)
+            {
+                dtUser u = new dtUser()
+                {
+                    Name = $"{item.Name} {item.Surname}",
+                    Email = item.Email,
+                    Department = item.Department,
+                    Office = item.Office
+                };
+
+                await this.repository.CreateAsync(u);
+            }
         }
     }
 }
---
+++ TiskWASM/Shared/Csv/csvKontakt.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiskWASM.Shared.Csv
+{
+    public class csvKontakt
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Surname { get; set; } = string.Empty;
+
+        public string Office { get; set; } = string.Empty;
+
+        public string Department { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+    }
+}
